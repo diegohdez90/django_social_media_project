ccd0c1f7d9bc9888b3a3274f716ee9ddf78ed136
diegohdez12@gmail.com <diegohdez12@gmail.com>
2019-01-04 19:08:34 -0600

    feat: add template tags to render groups info

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin,PermissionRequiredMixin
+from django.core.urlresolvers import reverse
+from django.views.generic import (
+                                    CreateView,
+                                    UpdateView,
+                                    DeleteView,
+                                    ListView,
+                                    DetailView,
+                                )
+from groups.models import Group,GroupMember
 
 # Create your views here.
+class  CreateView(LoginRequiredMixin):
+    fields = ('name','description')
+    model = Group
+
+class Singlegroup(DetailView):
+    model = Group
+
+class ListGroup(ListView):
+    model = Group
